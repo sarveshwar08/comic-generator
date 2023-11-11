9e0efb3728795b1b4e2a0ae9ee2aae0a3df736cd
sarveshwar08 <sarveshwar.pandey0805@gmail.com>
2023-11-11 16:01:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from './Components/Form';
+import React, { useRef, useState } from 'react';
 
 function App() {
+
+  const [imageUrls, setImageUrls] = useState([]);
+
+  const currentIndexOfImage = useRef(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col items-center justify-center md:h-screen md:flex-row bg-black'>
+      <div className="md:w-1/3">
+        <Form imageUrls={imageUrls} setImageUrls={setImageUrls} currentIndexOfImage={currentIndexOfImage} />
+      </div>
+      <div className='md:w-2/3 px-4'>
+        <div id="generated-images" className='flex flex-wrap justify-center'>
+          <div className='border border-gray-500 h-48 w-48 bg-black'>
+            {imageUrls && imageUrls[0] && (<img src={imageUrls[0]} alt='generatedImage' className='object-cover' />)}
+          </div>
+          <div className='border border-gray-500 h-48 w-48 bg-black'>
+            {imageUrls && imageUrls[1] && (<img src={imageUrls[1]} alt='generatedImage' className='object-cover' />)}
+          </div>
+          <div className='border border-gray-500 h-48 w-48 bg-black'>
+            {imageUrls && imageUrls[2] && (<img src={imageUrls[2]} alt='generatedImage' className='object-cover' />)}
+          </div>
+          <div className='border border-gray-500 h-48 w-48 bg-black'>
+            {imageUrls && imageUrls[3] && (<img src={imageUrls[3]} alt='generatedImage' className='object-cover' />)}
+          </div>
+          <div className='border border-gray-500 h-48 w-48 bg-black'>
+            {imageUrls && imageUrls[4] && (<img src={imageUrls[4]} alt='generatedImage' className='object-cover' />)}
+          </div>
+          <div className='border border-gray-500 h-48 w-48 bg-black'>
+            {imageUrls && imageUrls[5] && (<img src={imageUrls[5]} alt='generatedImage' className='object-cover' />)}
+          </div>
+          <div className='border border-gray-500 h-48 w-48 bg-black'>
+            {imageUrls && imageUrls[6] && (<img src={imageUrls[6]} alt='generatedImage' className='object-cover' />)}
+          </div>
+          <div className='border border-gray-500 h-48 w-48 bg-black'>
+            {imageUrls && imageUrls[7] && (<img src={imageUrls[7]} alt='generatedImage' className='object-cover' />)}
+          </div>
+          <div className='border border-gray-500 h-48 w-48 bg-black'>
+            {imageUrls && imageUrls[8] && (<img src={imageUrls[8]} alt='generatedImage' className='object-cover' />)}
+          </div>
+          <div className='border border-gray-500 h-48 w-48 bg-black'>
+            {imageUrls && imageUrls[9] && (<img src={imageUrls[9]} alt='generatedImage' className='object-cover' />)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Form.js
@@ -0,0 +1,123 @@
+import React, { useRef, useState } from 'react';
+import html2canvas from 'html2canvas';
+
+const Form = ({ imageUrls, setImageUrls, currentIndexOfImage }) => {
+    const [userInput, setUserInput] = useState('');
+    const [isGenerating, setIsGenerating] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+    const handleChange = (event) => {
+        setSelectedIndex(event.target.value - 1);
+    };
+
+    async function query(data) {
+        try{
+            const response = await fetch(
+                "https://xdwvg9no7pefghrn.us-east-1.aws.endpoints.huggingface.cloud",
+                {
+                    headers: {
+                        "Accept": "image/png",
+                        "Authorization": "Bearer VknySbLLTUjbxXAXCjyfaFIPwUTCeRXbFSOjwRiCxsxFyhbnGjSFalPKrpvvDAaPVzWEevPljilLVDBiTzfIbWFdxOkYJxnOPoHhkkVGzAknaOulWggusSFewzpqsNWM",
+                        "Content-Type": "application/json"
+                    },
+                    method: "POST",
+                    body: JSON.stringify(data),
+                }
+            );
+            const result = await response.blob();
+            return result;
+        } catch(error) {
+            alert("Error occured in generating image")
+        }
+    }
+
+    async function handleSubmitClick() {
+        // disabling the submit and export buttons
+        setIsGenerating(true);
+
+        // fetching image through query
+        const result = await query({ "inputs": userInput })
+        const newArray = [...imageUrls];
+        if (newArray.length > selectedIndex) {
+            // in case we are attemping to re-generate any existing image, then replace the url of the present index
+            newArray[selectedIndex] = URL.createObjectURL(result);
+        } else {
+            // push the new generated image url in the array
+            newArray.push(URL.createObjectURL(result))
+        }
+
+        setImageUrls(newArray);
+        setIsGenerating(false);
+
+        if (selectedIndex < 9) {
+            // since we are asked to support only 10 images at maximum
+            if (selectedIndex == currentIndexOfImage.current) {
+                const newIndex = currentIndexOfImage.current += 1;
+                setSelectedIndex(newIndex)
+            } else {
+                setSelectedIndex(currentIndexOfImage.current)
+            }
+
+        }
+    };
+
+    async function handleDownloadClick() {
+        // select the element containing the generated images
+        const element = document.getElementById('generated-images'),
+        // convert it to canvas for exporting
+            canvas = await html2canvas(element),
+            data = canvas.toDataURL('image/jpg'),
+            link = document.createElement('a');
+        link.href = data;
+        link.download = 'downloaded-image.jpg';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+    return (
+        <div className='mx-auto px-4'>
+
+            <div className='flex space-x-2 items-center'>
+                <h2 className='my-auto text-lg text-white'>Please enter your prompt for image</h2>
+                <div className='my-auto'>
+                    <select value={selectedIndex + 1} onChange={handleChange}>
+                        {Array.from({ length: currentIndexOfImage.current + 1 }, (_, index) => (
+                            <option key={index + 1} value={index + 1}>
+                                {index + 1}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+
+            <input
+                type="text"
+                onChange={(e) => { setUserInput(e.target.value) }}
+                value={userInput}
+                className='border p-1 rounded w-full my-2 focus:outline-none'
+            />
+
+            <div className='flex space-x-4 items-center my-4 flex-wrap'>
+                <button
+                    type="button"
+                    onClick={handleSubmitClick}
+                    className="border rounded bg-green-500 border-green-500 px-6 py-2 cursor-pointer text-white disabled:opacity-50 disabled:cursor-not-allowed"
+                    disabled={isGenerating || userInput === ''}
+                >
+                    Submit
+                </button>
+                <button
+                    type="button"
+                    onClick={handleDownloadClick}
+                    className="border rounded bg-slate-950 border-white px-6 py-2 cursor-pointer text-white disabled:opacity-50 disabled:cursor-not-allowed"
+                    disabled={imageUrls.length < 1}
+                >
+                    Export
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Form;
